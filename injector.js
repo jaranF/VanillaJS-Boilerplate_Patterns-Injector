e28ad2f72174e531d5aa7af9a4dbc42fdd5ea913
gituJaran <gituJaran-healthca17@currahee.co.uk>
2017-03-31 21:44:49 +0545

    Working injector but drops this reference to target function instead of using implicit binding

---
+++ injector.js
@@ -0,0 +1,20 @@
+if (!Function.prototype.inject) {
+  Function.prototype.inject = function(bindToObj, argsToInjectOriginal) { /* console.log('this + args.length = %d', arguments.length); console.dir(this); */
+    var argsToInject = [], fnToInjectInto =  this, toString = Object.prototype.toString, slice = Array.prototype.slice;
+    bindToObj = typeof bindToObj === "undefined" ? {} : bindToObj;
+    if (typeof argsToInjectOriginal === "undefined") { 
+      argsToInjectOriginal = bindToObj;
+      bindToObj = {};
+    }
+    if (toString.call(argsToInjectOriginal) === "[object Array]") {
+      argsToInject = argsToInject.concat(argsToInjectOriginal);
+    } else {
+      argsToInject[0] = argsToInjectOriginal;
+    }
+
+    return function() {
+      //inline direct params are received as args also as per usual. i.e. the arguments object.
+      fnToInjectInto.apply(bindToObj, argsToInject.concat(slice.call(arguments, 0)));
+    }
+  };
+}
