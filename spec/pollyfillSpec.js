f257ca086bb469e0eac2707e25b75d3f81dfdf22
Jaran F <jaranMcLarn@gmail.com>
2017-07-12 16:08:19 +0100

    Test for 'tddjs.isOwnProperty()' which is used to prevent creating a shadowing property on an object with the name 'hasOwnProperty' and foiling the native method.

--- spec/pollyfillSpec.js
+++ spec/pollyfillSpec.js
@@ -2,14 +2,36 @@
 /*jslint newcap: true, white: true, sloppy: true, vars: true, unparam: true */
 
 /**
- * @author jaranf
- * @description Authored on 12/07/17
+ * @author Christan Johansen
+ * @description C.J.'s xUnit assert framework style unit tests re-authored by me, JaranF on 12/07/17 for BDD Jasmine
  */
 
+describe("Pollyfill...tddjs.isOwnProperty", function () {
+  it("should only perform as expected even if there is an overwriting hash on the object with the name 'hasOwnProperty'.", function () {
+    var person = {
+      name: "Christian",
+      profession: "Programmer",
+      location: "Norway"
+    };
+    person["hasOwnProperty"] = function() {
+      throw Error("We've shadowed the native \'hasOwnProperty\' method [usually available in the prototype chain] with our own code");
+    };
+    var result = [];
+    for (var prop in person) {
+      if (person.hasOwnProperty(prop)) {
+        result.push(prop);
+      }
+    }
+    var expected = ["location", "name", "profession"];
+    expect(result.sort()).toBe(expected);
+  });
+
+});
+
 describe("Pollyfill...tddjs.each", function () {
-  it("should @TODO", function () {
+  it("should only iterate over own properies when tested in a loop", function () {
     var obj = "a_value";
-    expect(obj)..nottoEqual("a_value");
+    expect(obj).not.nottoEqual("a_value");
   });
 
 });
