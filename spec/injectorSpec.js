52da760e6e4f416089444d32359c986cac714932
Jaran F <jaranMcLarn@gmail.com>
2017-07-13 11:45:24 +0100

    Tests for demonstrating non-interference with the function/method's signature/params once stuff is injected into it

--- spec/injectorSpec.js
+++ spec/injectorSpec.js
@@ -11,7 +11,7 @@ describe("InjectorJs", function () {
     if (this.__lives <= 0) {
       return "You have expired you now are an \'ex-" + animal + "\'";
     }
-    return "The " + animal + " " + this.__lives + sLivesMsg;
+    return "The " + animal + " has " + this.__lives + sLivesMsg;
   } //end fn 'stPetersReckoner()'
 
   function returnThis() {
@@ -30,6 +30,18 @@ describe("InjectorJs", function () {
   it("should inject params that get seen as \'__\' suffixed properties on the \'this\' object", function () {
     var result = returnThis.inject({xhr: "xhr"}).andExecuteWith();
     expect(result.__xhr).toBeDefined();
+  });
+  it("should make injected params available without interfering with function/method's signature", function () {
+    expect(stPetersReckoner.length).toEqual(2); //Two params define the functions signature (i.e. named arguments 'animal' and 'sLivesMsg'
+    var apiAfterInjection = stPetersReckoner.inject({lives: 0});
+    expect(stPetersReckoner.length).toEqual(2); //Still the same, all good.
+    var result = apiAfterInjection.andExecuteWith("cat", " lives remaining");
+    expect(result).toEqual("You have expired you now are an \'ex-cat\'");
+  });
+  it("should make injected params available without interfering with function/method's params", function () {
+    var apiAfterInjection = stPetersReckoner.inject({lives: 9});
+    var result = apiAfterInjection.andExecuteWith("cat", " lives remaining");
+    expect(result).toEqual("The cat has 9 lives remaining");
   });
 });
  
