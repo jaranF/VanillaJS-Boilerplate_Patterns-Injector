649a4c2c461224909b8f5c9d60714adc36255049
Jaran F <jaranMcLarn@gmail.com>
2017-07-13 05:51:10 +0100

    New functionality injector working correctly (injects on to obj accessible via this) but not fully tested

--- injector.js
+++ injector.js
@@ -1,21 +1,48 @@
 if (!Function.prototype.inject) {
-  Function.prototype.inject = function(bindToObj, argsToInjectOriginal) {
-    var argsToInject = [], fnToInjectInto =  this, toString = Object.prototype.toString, slice = Array.prototype.slice;
-    bindToObj = typeof bindToObj === "undefined" ? {} : bindToObj;
-    if (typeof argsToInjectOriginal === "undefined") {
-      argsToInjectOriginal = bindToObj;
-      bindToObj = {};
+  Function.prototype.inject = function(injectionDefinition) {
+    var arrArgsToInject, fnToInjectInto =  this, objInjectedOnto;
+    var bindingSuffix = "__", errorIfOverwrite =false;
+    var toString = Object.prototype.toString, slice = Array.prototype.slice;
+
+    function iterateBindItems(colThingsToInject, objAttachTo) {
+      tddjs.each(colThingsToInject, function(itemKeyName, itemValue) {
+        if (toString.call(itemValue) === "[object Array]" || toString.call(itemValue) === "[object Object]") {
+          iterateBindItems(itemValue, objAttachTo);
+        } else {
+          objAttachTo[bindingSuffix + itemKeyName] = itemValue;
+        }
+      });
     }
-    if (toString.call(argsToInjectOriginal) === "[object Array]") {
-      argsToInject = argsToInject.concat(argsToInjectOriginal);
-    } else {
-      argsToInject[0] = argsToInjectOriginal;
+
+    if (toString.call(injectionDefinition) === "[object Object]" && injectDefinition.bindToObject) {
+      objInjectedOnto = injectionDefinition.bindToObject;
+      errorIfOverwrite = !!injectionDefinition.errorIfOverwrite;
+      bindingSuffix = injectionDefinition.bindingSuffix || bindingSuffix;
+      arrArgsToInject = slice.call(arguments, 1);
     }
+    else {
+      objInjectedOnto = {};
+      arrArgsToInject = slice.call(arguments, 0);
+    }
+    debugger;
+    iterateBindItems(arrArgsToInject, objInjectedOnto);
     return {
       andExecuteWith: function() {
         //inline direct params are received as args as per usual. i.e. the arguments object.
-        return fnToInjectInto.apply(bindToObj, argsToInject.concat(slice.call(arguments, 0)));
+        return fnToInjectInto.apply(objInjectedOnto, slice.call(arguments, 0));
       }
     };
   };
 }
+//var myObj.func.inject({lives: 9}).andExecuteWith("cats", "lives remaining:");
+/*function stPetersReckoner(animal, sLivesMsg) {
+ }
+ stPetersReckoner.inject([{lives: 9}, {lifeLivedWell: true}]).andExecuteWith("cats", "lives remaining:");
+ //injects 9 and {lifeLivedWell: true} onto the this object that the stPetersReckoner sees
+ stPetersReckoner.inject({bindToObj: {}, bindingSuffix: "__", errorIfOverwrite: false}, {lives: 9}, {lifeLivedWell: true}).andExecuteWith("cats", "lives remaining:");
+ //injects 9 and {lifeLivedWell: true} onto the this object that the stPetersReckoner sees
+ stPetersReckoner.inject([{lives: 9}, {lifeLivedWell: true}]).andExecuteWith("cats", "lives remaining:");
+ //injects 9 and {lifeLivedWell: true} onto the this object that the stPetersReckoner sees#
+ stPetersReckoner.inject({bindToObj: {}, bindingSuffix: "__", errorIfOverwrite: false}, [ {lives: 9}, {lifeLivedWell: true} ]).andExecuteWith("cats", "lives remaining:");
+ //injects 9 and {lifeLivedWell: true} onto the this object that the stPetersReckoner sees
+ */
